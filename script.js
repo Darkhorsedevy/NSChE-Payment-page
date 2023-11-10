a0f46a1f2cc97f7fecb49c701466b8bea1cf5dc9
Darkhorsedevy <111581640+Darkhorsedevy@users.noreply.github.com>
2023-11-10 22:47:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const makePaymentBtn = document.getElementById("submit-btn");
+const selectLevel = document.getElementById("selectLevel");
+
+selectLevel.onchange = function (params) {
+    if (selectLevel.value == "null") {
+        makePaymentBtn.style.backgroundColor = "lightgray";
+        makePaymentBtn.disabled = true;
+    } else {
+        makePaymentBtn.style.backgroundColor = "green";
+        makePaymentBtn.disabled = false;
+    }
+}
+
+function changeColor (params) {
+    document.body.style.backgroundColor = "green";
+}
